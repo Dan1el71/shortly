fbccc32f717ebba663c81f597ee31e495ec95c5e
Dan1el71 <ddrmhk1@gmail.com>
2024-02-01 12:39:55 -0500

    🔨 Type notations

--- server/resolvers/dateScalar.ts
+++ server/resolvers/dateScalar.ts
@@ -3,19 +3,19 @@ import { GraphQLScalarType, Kind } from 'graphql'
 export default new GraphQLScalarType({
   name: 'Date',
   description: 'Date custom scalar type',
-  serialize(value) {
+  serialize(value: any) {
     if (value instanceof Date) {
       return value.getTime()
     }
     throw new Error('GraphQL Date Scalar serializer expected a `Date` object')
   },
-  parseValue(value) {
+  parseValue(value: any) {
     if (typeof value === 'number') {
       return new Date(value)
     }
     throw new Error('GraphQL Date Scalar parser expected a `number`')
   },
-  parseLiteral(ast) {
+  parseLiteral(ast: any) {
     if (ast.kind === Kind.INT) {
       return new Date(parseInt(ast.value, 10))
     }
