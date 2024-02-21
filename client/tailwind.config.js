c95b7c0881fda7eeaac405800ed0ac1e2f3637a0
Dan1el71 <ddrmhk1@gmail.com>
2024-02-21 13:39:03 -0500

    💄 Colors style

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#0d1117",
+        lightDark: "#161b22",
+      }
+    },
   },
   plugins: [],
 }
