f5c406e99064814828c1a54f2c8fca64bee71d1b
Dan1el71 <ddrmhk1@gmail.com>
2024-02-28 13:38:41 -0500

    ✨ Email Login

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -3,14 +3,116 @@ import { Request, Response } from 'express'
 import { handleError } from '../utils/handleError'
 import { uniqueNamesGenerator } from 'unique-names-generator'
 import prisma from '../db'
-import { JWT_SECRET, nameGeneratorConfig } from '../config'
+import { JWT_SECRET, NODE_ENV, nameGeneratorConfig } from '../config'
 
-export const emailLoginHandler = (req: Request, res: Response) => {
-  res.send('login')
+export const emailLoginHandler = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+      console.log(email, password)
+      return res.status(400).json({
+        message: 'All fields required',
+      })
+    }
+
+    const account = await prisma.account.findFirst({
+      where: {
+        email,
+      },
+    })
+
+    if (account) {
+      const validPassword = await prisma.account.validPassword(
+        password,
+        account.password
+      )
+
+      const user = await prisma.user.findFirst({
+        where: { userAcountId: account.uuid },
+      })
+
+      if (validPassword && user) {
+        const token = jwt.sign(
+          { _id: user.uuid, username: user.username },
+          JWT_SECRET,
+          { expiresIn: '1d' }
+        )
+
+        return res
+          .cookie('auth', token, {
+            expires: new Date(Date.now() + 86400000),
+            maxAge: 86400000,
+            httpOnly: true,
+            secure: NODE_ENV === 'production',
+          })
+          .status(200)
+          .json({
+            message: 'Logged in successfully!',
+          })
+      }
+    }
+
+    return res.status(400).json({
+      status: 'Unauthorized',
+      message: 'Invalid credentials',
+    })
+  } catch (err) {
+    handleError(res, err)
+  }
 }
 
-export const registerEmail = (req: Request, res: Response) => {
-  res.send('register')
+export const registerEmail = async (req: Request, res: Response) => {
+  try {
+    const { email, username, password } = req.body
+
+    if (!email || !username || !password) {
+      return res.status(400).json({
+        message: 'Please fill in all fields',
+      })
+    }
+
+    const userExists = await prisma.user.findFirst({ where: { username } })
+    const emailExists = await prisma.account.findFirst({ where: { email } })
+    if (userExists || emailExists) {
+      return res.status(400).json({
+        message: 'User already exists',
+      })
+    }
+
+    const account = await prisma.account.createAccount(email, password)
+    const user = await prisma.user.create({
+      data: {
+        username,
+        userAcountId: account.uuid,
+      },
+    })
+
+    const token = jwt.sign(
+      {
+        _id: user.uuid,
+        username: user.username,
+      },
+      JWT_SECRET,
+      {
+        expiresIn: '1d',
+      }
+    )
+
+    res
+      .cookie('auth', token, {
+        expires: new Date(Date.now() + 86400000),
+        maxAge: 86400000,
+        httpOnly: true,
+        secure: NODE_ENV === 'production',
+      })
+      .status(201)
+      .json({
+        message: 'Registered successfully!',
+      })
+  } catch (error) {
+    handleError(res, error)
+  }
 }
 
 export const guestLogin = async (req: Request, res: Response) => {
--- server/db.ts
+++ server/db.ts
@@ -1,5 +1,27 @@
 import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient().$extends({
+  model: {
+    account: {
+      async createAccount(email: string, password: string) {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        return await prisma.account.create({
+          data: {
+            email,
+            provider: 'Email',
+            password: hashedPassword,
+          },
+        })
+      },
+      async validPassword(password: string, hashedPassword: string | null) {
+        if (hashedPassword)
+          return await bcrypt.compare(password, hashedPassword)
+      },
+    },
+  },
+})
 
 export default prisma
